4af4dd4e0d7c6d88ce4c4ba1952583cd39bc28ff
871466565@qq.com <871466565@qq.com>
2021-01-08 21:21:12 +0800

    zk server、zk client源码 搭建服务提交

---
+++ src/main/java/org/apache/zookeeper/version/Info.java
@@ -0,0 +1,9 @@
+package org.apache.zookeeper.version;
+public interface Info {
+    int MAJOR = 3;
+    int MINOR = 5;
+    int MICRO = 6;
+    String QUALIFIER = null;
+    String REVISION_HASH = "c11b7e26bc554b8523dc929761dd28808913f091";
+    String BUILD_DATE = "10/08/2019 20:18 GMT";
+}
